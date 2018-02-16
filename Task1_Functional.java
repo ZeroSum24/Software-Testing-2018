8e9263478e2647193d9e6a10eb8840280acea706
ColinParrott <s1546623@sms.ed.ac.uk>
2018-02-16 19:43:54 +0000

    EntryMap.store() tests implemented

--- Task1_Functional.java
+++ Task1_Functional.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -21,7 +22,9 @@ public class Task1_Functional
         engine = new TemplateEngine();
         simpleEngine = new SimpleTemplateEngine();
     }
-    
+
+    // Spec 1 ------------------------------
+
     @Test(expected = RuntimeException.class)
     public void EntryMapTemplateNull()
     {
@@ -33,5 +36,52 @@ public class Task1_Functional
     {
     	map.store("", "text");
     }
-    
+    // -------------------------------------
+
+    // Spec 2 ------------------------------
+
+    @Test(expected = RuntimeException.class)
+    public void EntryMapReplaceValueNull()
+    {
+        map.store("ff", null);
+    }
+    // --------------------------------------
+
+    // Spec 3 ------------------------------
+    @Test
+    public void EntryMapOrderCorrect()
+    {
+        map.store("name", "Adam");
+        map.store("age", "19");
+        map.store("city", "Edinburgh");
+
+        String output = engine.evaluate("${name} is ${age} from ${city}", map, TemplateEngine.DELETE_UNMATCHED);
+        assertEquals(output, "Adam is 19 from Edinburgh");
+    }
+
+     // -------------------------------------
+
+     // Spec 4 ------------------------------
+    @Test
+    public void DuplicateEntriesSize()
+    {
+        map.store("name", "Adam");
+        map.store("name", "Bob");
+        assertEquals(map.getEntries().size(), 1);
+    }
+
+    @Test
+    public void DuplicateEntriesCorrect()
+    {
+        map.store("name", "Adam");
+        map.store("name", "Bob");
+        map.store("age", "19");
+        map.store("city", "Edinburgh");
+        map.store("city", "London");
+
+        String output = engine.evaluate("${name} is ${age} from ${city}", map, TemplateEngine.DELETE_UNMATCHED);
+        assertEquals(output, "Adam is 19 from Edinburgh");
+    }
+    // -------------------------------------
+
 }
