2a1a4ec2c9820a7b688ff883cf5423ef92537398
ZeroSum24 <stephen_waddell@hotmail.co.uk>
2018-02-16 13:52:45 +0000

    Added Wee Colz wee tests

---
+++ FunctionalTests.java
@@ -0,0 +1,37 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import st.EntryMap;
+import st.TemplateEngine;
+import st.SimpleTemplateEngine;
+
+// Colin Parrott (s1546623) and Stephen Waddell (s1346249)
+
+public class FunctionalTests 
+{
+    private EntryMap map;
+    private TemplateEngine engine;
+    private SimpleTemplateEngine simpleEngine;
+    
+    @Before
+    public void setUp() throws Exception 
+    {
+        map = new EntryMap();
+        engine = new TemplateEngine();
+        simpleEngine = new SimpleTemplateEngine();
+    }
+    
+    @Test(expected = RuntimeException.class)
+    public void EntryMapTemplateNull()
+    {
+    	map.store(null, "text");
+    }
+    
+    @Test(expected = RuntimeException.class)
+    public void EntryMapTemplateEmpty()
+    {
+    	map.store("", "text");
+    }
+    
+}
